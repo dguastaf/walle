29515b7b5af0ce694e3b5cbefb805cf6066a1199
Doug Guastaferro <dguastaferro@gmail.com>
2017-12-22 22:03:10 -0800

    Update action param

--- Wall-e/Managers/NetworkManager.swift
+++ Wall-e/Managers/NetworkManager.swift
@@ -11,13 +11,14 @@ import Alamofire
 
 class NetworkManager {
     private static let BaseUrl = "http://192.168.1.12:5000"
+    private static let KeyActionParam = "action"
     
     static func sendAction(device: Device, deviceAction: DeviceAction) {
         let devicePath = device.endpoint
         let deviceActionParam = deviceAction.actionParam
         
         let json: Parameters = [
-            "action": deviceActionParam
+            KeyActionParam: deviceActionParam
         ]
         
         print("Sending \(deviceAction.actionParam) request to \(device.displayName)")
@@ -25,8 +26,12 @@ class NetworkManager {
         Alamofire.request("\(NetworkManager.BaseUrl)/\(devicePath)",
             method: .post,
             parameters: json,
-            encoding: JSONEncoding.default).response { (response) in
-                print(response)
+            encoding: URLEncoding.queryString)
+            .validate()
+            .response { (response) in
+                if response.response?.statusCode != 200 {
+                    print (response)
+                }
         }
     }
 }
